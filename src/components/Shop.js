a9b496c3e572b7cc41ca6533439b1c788cd2f4a8
David Puerta Martos <dpuerta@usal.es>
2022-01-02 01:35:40 +0100

    shop functionality finished

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Offcanvas, Button } from 'react-bootstrap'
 
 const initialItems = [
     {id: 1, price: 25}, {id: 2, price: 24}, 
@@ -9,15 +10,70 @@ const initialItems = [
     {id: 11, price: 25}, {id: 12, price: 88}
   ]
 
+  const options = [
+    {
+      name: 'checkout',
+      scroll: true,
+      backdrop: true,
+      placement: "end"
+    }
+  ];
+
+
+function OffCanvasCartShop({ name, itemsCart, numberOfItems, totalPrice, ...props }) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    
+
+    return (
+        <>
+        <Button variant="dark" onClick={handleShow} className="me-2">
+            {name}
+        </Button>
+        <Offcanvas show={show} onHide={handleClose} {...props}>
+            <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Checkout</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <p>There are {numberOfItems} items in the cart</p>
+                <ul>
+                {itemsCart.map(element=>
+                    <li key={element.item.id}>
+                        id: {element.item.id} {element.item.price}€/ud x {element.quantity} uds. 
+                        subtotal: {element.item.price * element.quantity}€
+                    </li>
+                )
+                }
+                </ul>
+                <p>Total price of {totalPrice}€</p>
+            </Offcanvas.Body>
+        </Offcanvas>
+        </>
+    );
+}
+
 const Shopcart = ({itemsCart}) => {
     const reducer = (previousitem, currentitem) => previousitem + currentitem;
+    const totalPrice = itemsCart.map(element=>element.item.price*element.quantity).reduce(reducer,0)
+    const numberOfItems = itemsCart.map(element=>element.quantity).reduce(reducer,0)
+    
     
     return(
         <div>
-            <div>Shopcart {itemsCart.length} total price: {itemsCart.map(item=>item.price).reduce(reducer,0)}€ <button>checkout</button></div>
+            <div>Shopcart {numberOfItems} total price: {totalPrice}€ 
+            {options.map((props, idx) => (
+                <OffCanvasCartShop key={idx} itemsCart={itemsCart} numberOfItems={numberOfItems} totalPrice={totalPrice} {...props} />
+            ))}   
+            </div>
         </div>
+
     )
 }
+
+
 
 const ItemDisplay = ({items, itemsCart, setItemsCart}) => {
     return (
@@ -31,13 +87,18 @@ const Item = ({itemToDisplay, itemsCart, setItemsCart}) => {
     const [itemNumber, setItemNumber] = useState(1)
     
     const handleAddToCart = async (n) => {
-        let output = []
-        for(let i=0; i<n; i++){
-            output = [...output, itemToDisplay]
-
+        const selectedItem = itemsCart.filter(e=>e.item.id===itemToDisplay.id)
+        const newObject = {
+            item: itemToDisplay,
+            quantity: n
         }
-        
-        setItemsCart(itemsCart.concat(output))
+        if(selectedItem.length===1){
+            setItemsCart(itemsCart.map(element=>element.item.id===itemToDisplay.id?newObject:element))
+        }
+        else{
+           
+            setItemsCart(itemsCart.concat(newObject))
+        }
         setItemNumber(0)
     }
 
